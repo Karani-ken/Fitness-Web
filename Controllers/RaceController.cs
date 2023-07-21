c27a4c7d9ed7579aabfd302fd7ba7cdccde0d87e
Karani-ken <academice79@gmail.com>
2023-07-21 11:50:12 +0300

    created the view and controller for races and fetched the data from the database

--- Controllers/RaceController.cs
+++ Controllers/RaceController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fitness_Web.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Fitness_Web.Controllers
 {
     public class RaceController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var races = _context.Races.ToList();
+            return View(races);
         }
     }
 }
