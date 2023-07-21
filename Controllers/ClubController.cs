102b8dea0fb4f1647ad87161d9d6670f48bafe02
Karani-ken <academice79@gmail.com>
2023-07-21 11:20:38 +0300

    created some changes on clubcontroller

--- Controllers/ClubController.cs
+++ Controllers/ClubController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fitness_Web.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Fitness_Web.Controllers
 {
     public class ClubController : Controller
     {
+       private readonly ApplicationDbContext _context;
+        public ClubController(ApplicationDbContext contex)
+        {
+                _context = contex;
+        }
         public IActionResult Index()
         {
-            return View();
+            var clubs = _context.Clubs.ToList();
+            return View(clubs);
         }
     }
 }
