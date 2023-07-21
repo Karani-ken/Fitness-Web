9418bb9cab3bb84c4e1910352cb1baf6952c2f9b
Kenneth Karani <Kenneth Karani@KARANI-KEN>
2023-07-21 10:57:38 +0300

    created views

---
+++ Controllers/ClubController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fitness_Web.Controllers
+{
+    public class ClubController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
