00b813c4fbe20e13b6b573579d0218918625cb25
Karani-ken <academice79@gmail.com>
2023-07-22 09:46:02 +0300

    added interface and repository folders and Iclubrepository.cs file and clubrepository.cs file

---
+++ Interfaces/IClubRepository.cs
@@ -0,0 +1,15 @@
+﻿using Fitness_Web.Models;
+
+namespace Fitness_Web.Interfaces
+{
+    public interface IClubRepository
+    {
+        Task<IEnumerable<Club>> GetAll();
+        Task<Club> GetByIdAsync(int id);
+        Task<IEnumerable<Club>> GetClubByCity(string city);
+        bool Add(Club club);
+        Task Update(Club club);
+        Task Delete(Club club);
+        bool Save();
+    }
+}
---
+++ Repository/ClubRepository.cs
@@ -0,0 +1,49 @@
+﻿using Fitness_Web.Data;
+using Fitness_Web.Interfaces;
+using Fitness_Web.Models;
+
+namespace Fitness_Web.Repository
+{
+    public class ClubRepository : IClubRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public ClubRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public bool Add(Club club)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(Club club)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Club>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Club> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Club>> GetClubByCity(string city)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Club club)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
