3ca4a27034c111a1ae276175856004b3d341dd34
Karani-Ken <academice79@gmail.com>
2023-07-30 16:21:27 +0300

    created the editrace view model page

---
+++ ViewModels/EditRaceViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Fitness_Web.Data.Enum;
+using Fitness_Web.Models;
+
+namespace Fitness_Web.ViewModels
+{
+    public class EditRaceViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public IFormFile Image { get; set; }
+        public string? URL { get; set; }
+        public int AddressId { get; set; }
+        public Address Address { get; set; }
+        public RaceCategory RaceCategory { get; set; }
+    }
+}
